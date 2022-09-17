5353580bbc8cf7a9ba0450519b0a834fdf38503f
Farrel Mahaztra <15523645+farrelmahaztra@users.noreply.github.com>
2022-09-17 21:56:25 +0700

    feat: add barebones value struct

--- main.go
+++ main.go
@@ -4,6 +4,27 @@ import (
 	"fmt"
 )
 
+type Value struct {
+	Data  float32
+	Grad  float32
+	Label string
+	Op    string
+	Prev  []*Value
+}
+
+func NewValue(data float32) *Value {
+	value := &Value{
+		Data:  data,
+		Grad:  0.0,
+		Label: "",
+		Op:    "",
+		Prev:  nil,
+	}
+
+	return value
+}
+
 func main() {
-	fmt.Println("Hello world!")
+	x := NewValue(1.0)
+	fmt.Println(x)
 }
